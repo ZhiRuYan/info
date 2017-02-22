d423b1526173defe46d8c2b3b23a27e312cd205e
unknown <>
2017-02-22 10:39:00 +0800

    update

--- deploy/app.js
+++ deploy/app.js
@@ -20,7 +20,7 @@ app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
-	extended: false
+  extended: false
 }));
 app.use(cookieParser());
 
@@ -36,28 +36,30 @@ var routers = glob.sync(__dirname + '/routes/**/*.js');
 routers.forEach(function (route) {
   app.use(require(route));
 });
-//------------------------
+
+//将所有路由转发到index.html
+
 app.get('*.html', function (req, res) {
-  console.log('----------------------')
-  console.log('----------------------')
-  res.sendfile('../public/index.html');
+  res.sendFile(__dirname + '/public/index.html');
 });
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-	var err = new Error('Not Found');
-	err.status = 404;
-	next(err);
+app.use(function (req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-	// set locals, only providing error in development
-	res.locals.message = err.message;
-	res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-	// render the error page
-	res.status(err.status || 500);
-	res.render('error');
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
 });
 
+
+app.listen(9090);
 module.exports = app;
